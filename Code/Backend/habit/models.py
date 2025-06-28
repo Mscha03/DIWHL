1e0796eb78744dc984246b3af7bfd09e2745d168
mscha <m.saleh.sedeghi@gmail.com>
2025-06-28 09:54:30 +0330

    Habit model created

--- Code/Backend/habit/models.py
+++ Code/Backend/habit/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Habit(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    state_mode = models.CharField(max_length=20)
+    frequency = models.CharField(max_length=7)
+    has_pattern = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class HabitPattern(models.Model):
+    habit = models.ForeignKey(to=Habit, on_delete=models.CASCADE)
+    day = models.IntegerField()
+
+class HabitLog(models.Model):
+    habit = models.ForeignKey(to=Habit, on_delete=models.CASCADE)
+    date = models.DateField()
+    completion_state = models.IntegerField()
